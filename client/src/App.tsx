1a4b21282d5971753a806a4c7ee00c1ac5a28586
wizardcloth <151219131+wizardcloth@users.noreply.github.com>
2025-07-09 15:48:22 +0530

    login & register frontend done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import { Button } from "./components/ui/button"
+import { Route, Routes } from "react-router-dom"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/registerpage"
 function App() {
 
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button variant={"destructive"}>Click me</Button>
-      </div>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+      </Routes>
     </>
   )
 }
---
+++ client/src/pages/RegisterPage.tsx
@@ -0,0 +1,72 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+function RegisterPage() {
+    return (
+        <div className="relative min-h-screen flex items-center">
+            <div 
+                className="absolute inset-0 bg-cover bg-center bg-no-repeat filter blur-xs"
+                style={{ backgroundImage: `url('/bg.jpg')` }}
+            />
+            
+            <div className="relative rounded-xl shadow-2xl/60 backdrop-blur-sm p-10 w-full max-w-md flex flex-col gap-4 mx-auto">
+                <h1 className="text-3xl font-bold">Sign In</h1>
+                <p className="text-sm text-gray-500 mb-4">
+                    Continue to access your dashboard
+                </p>
+
+                <Button className="w-full flex gap-2 bg-emerald-500 hover:bg-emerald-600 hover:cursor-pointer">
+                    <span>🔍</span> Sign in with Google
+                </Button>
+
+                <div className="flex items-center my-4">
+                    <div className="flex-grow border-t border-gray-300" />
+                    <span className="mx-2 text-xs text-gray-500">OR</span>
+                    <div className="flex-grow border-t border-gray-300" />
+                </div>
+
+                <div className="space-y-2">
+                    <Label htmlFor="Name" className="text-xs">Email</Label>
+                    <Input
+                        id="Name"
+                        type="Name"
+                        placeholder="Enter your Full Name"
+                        className="w-full border-gray-400 border-2"
+                    />
+                    <Label htmlFor="email" className="text-xs">Email</Label>
+                    <Input
+                        id="email"
+                        type="email"
+                        placeholder="Enter your email"
+                        className="w-full border-gray-400 border-2"
+                    />
+                
+                    <Label htmlFor="password" className="text-xs">Password</Label>
+                    <Input
+                        id="password"
+                        type="password"
+                        placeholder="Create password"
+                        className="w-full  border-gray-400 border-2"
+                    />
+                    {/* <div className="text-right">
+                        <a href="#" className="text-xs text-gray-500 hover:underline">
+                            Forgot Password?
+                        </a>
+                    </div> */}
+                </div>
+
+                <Button className="w-full hover:cursor-pointer">Sign In</Button>
+
+                <p className="text-xs text-center text-gray-600">
+                    Already have an account? {""}
+                    <Link to="/" className="text-black font-semibold underline">
+                        Sign In 
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default RegisterPage;
